d9f514dacb4b1c58626a87ea7c1dc6de509011b8
Arleigh Mitchell <arleighmitchell@gmail.com>
2016-09-27 13:48:41 -0700

    Add question one

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
 
+//question one
+	
+	function alternateString(size){
+	    var num = ""
+
+	    for (var i = 0; i < size; i++){
+	        
+	        if (num.slice(-1) === "1"){
+	            num = num.concat("0");
+	        } else {
+	            num = num.concat("1");
+	        };
+	    };
+	    return num;
+	};
+	console.log(alternateString(5));
+
+//question two
+
+	
+
+
+
+
 });
